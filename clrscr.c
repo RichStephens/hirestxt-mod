4fe4348528c943da44168f5877c0709469d4d2af
Rich Stephens <richswww@gmail.com>
2025-10-04 12:15:26 -0400

    Initial checkin

---
+++ clrscr.c
@@ -0,0 +1,14 @@
+/*  clrscr.c
+
+    By Pierre Sarrazin <http://sarrazip.com/>
+    This file is in the public domain.
+*/
+
+#include "hirestxt_private.h"
+
+
+void clrscr(void)
+{
+    home();
+    clear();
+}
